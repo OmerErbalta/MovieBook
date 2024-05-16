f15ff91e556491d7d2c508e260a52882ca06a192
Omer Erbalta <erbalta80@gmail.com>
2024-05-16 23:15:55 +0300

    Initial Commit

---
+++ LoodosProject/LoodosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoodosProjectApp.swift
+//  LoodosProject
+//
+//  Created by OmerErbalta on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoodosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
